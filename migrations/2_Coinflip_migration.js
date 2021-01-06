0a26761c9f537bccdcedd6b9dcf547a7e4462b63
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2021-01-06 17:18:52 +0530

    Update 2_Coinflip_migration.js

--- migrations/2_Coinflip_migration.js
+++ migrations/2_Coinflip_migration.js
@@ -5,5 +5,5 @@ module.exports = async function(deployer, network, accounts ) {
   const instance = await Coinflip.deployed();
   await instance.addMoney({from: accounts[0], value: web3.utils.toWei("0.5", "ether")});
 
-  console.log("The contract balance is ${await instance.balance}");
+  console.log("The contract balance is ${await instance.balance()}");
 };
